0b9b39c183752c0724885bdefcdf490aed4dd9b6
karantos <jan.repar@outlook.com>
2023-12-24 22:04:17 +0100

    Add project files.

---
+++ FaultCatalogAPI/Models/Fault.cs
@@ -0,0 +1,12 @@
+﻿namespace FaultCatalogAPI.Models
+{
+    public class Fault
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        // Basic many to many (relationship is mapped by convention in Entity Framework)
+        public List<SuccessCriterion> SuccessCriterions { get; set; } = new();
+    }
+}
---
+++ FaultCatalogAPI/Models/SuccessCriterion.cs
@@ -0,0 +1,14 @@
+﻿namespace FaultCatalogAPI.Models
+{
+    public class SuccessCriterion
+    {
+        public string RefId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string URL { get; set; }
+        public string Level { get; set; }
+
+        // Basic many to many- (relationship is mapped by convention in Entity Framework)
+        public List<Fault> Faults { get; set; } = new();
+    }
+}
