41cbed4739d0b6ea7f7d4143b72efb6e24a38977
Jan Repar <jan.repar@outlook.com>
2023-12-25 21:43:15 +0100

    Added database context class.

---
+++ FaultCatalogAPI/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using FaultCatalogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaultCatalogAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=faultcatalogdb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<Fault> Faults { get; set; }
+
+        public DbSet<SuccessCriterion> SuccessCriteria { get; set; }
+    }
+}
