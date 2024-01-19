d803d9bf206923f3f9bef3ab86eddb238cffc983
Jan Repar <jan.repar@outlook.com>
2024-01-19 22:05:39 +0100

    Updated Auth service.

--- FaultCatalogAPI/Services/AuthServices/AuthService.cs
+++ FaultCatalogAPI/Services/AuthServices/AuthService.cs
@@ -1,6 +1,13 @@
 ﻿using FaultCatalogAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace FaultCatalogAPI.Services.AuthServices
 {
@@ -27,6 +34,65 @@ namespace FaultCatalogAPI.Services.AuthServices
             return user;
         }
 
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            // Returns null if username or password incorrect
+            if (user.UserName != request.UserName || !VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+
+            string token = CreateToken(user);
+
+            var refreshToken = GenerateRefreshToken();
+            SetRefreshToken(refreshToken);
+
+            return token;
+        }
+
+        private RefreshToken GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddDays(7),
+                Created = DateTime.Now
+            };
+
+            return refreshToken;
+        }
+
+        private void SetRefreshToken(RefreshToken refreshToken)
+        {
+            user.RefreshToken = refreshToken.Token;
+            user.TokenCreated = refreshToken.Created;
+            user.TokenExpires = refreshToken.Expires;
+
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(5),
+                signingCredentials: credentials);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
         // Method to create password hash that uses HMACSHA512 hash algorithm
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
@@ -34,6 +100,16 @@ namespace FaultCatalogAPI.Services.AuthServices
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        // Method that verifies password hash
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
             }
         }
     }
--- FaultCatalogAPI/Services/AuthServices/IAuthService.cs
+++ FaultCatalogAPI/Services/AuthServices/IAuthService.cs
@@ -1,6 +1,17 @@
-﻿namespace FaultCatalogAPI.Services.AuthServices
+﻿using FaultCatalogAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaultCatalogAPI.Services.AuthServices
 {
     public interface IAuthService
     {
+        Task<ActionResult<User>> Register(UserDto request);
+        Task<ActionResult<string>> Login(UserDto request);
+        Task<ActionResult<string>> RefreshToken();
+        RefreshToken GenerateRefreshToken();
+        void SetRefreshToken(RefreshToken refreshToken);
+        string CreateToken(User user);
+        void CreatePasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
+        bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
     }
 }
