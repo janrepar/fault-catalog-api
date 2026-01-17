426fafc32ca8151807d838f3f9322b3a5dc27b35
Jan Repar <jan.repar@outlook.com>
2026-01-17 18:24:49 +0100

    Added health and liveness endpoints.

---
+++ FaultCatalogAPI/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FaultCatalogAPI.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HealthController : Controller
+    {
+        [HttpGet("health")]
+        public IActionResult GetHealth() => Ok("Healthy");
+
+        [HttpGet("liveness")]
+        public IActionResult GetLiveness() => Ok("Live");
+    }  
+}
